02a8434fe5bc964aab219a2ef01399890f6357ba
marlonabeykoon <marlonabeykoon@gmail.com>
2019-02-14 11:01:44 +0530

    sprint 3.0.0.4 DB changes

---
+++ alter_CSDB_CallCDRs.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public."CSDB_CallCDRs"
+ADD COLUMN "QueuePriority" integer;
---
+++ alter_DB_CAMP_Configurations.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public."DB_CAMP_Configurations"
+ADD COLUMN NumberLoadingMethod character varying(255)
+
